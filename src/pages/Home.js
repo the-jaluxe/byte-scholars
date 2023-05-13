02579ea29853e15ef290f8119ec4b5d7a7554416
Jaluxe <jcornel00@gmail.com>
2023-05-13 03:06:54 -0700

    Created pages and component folders, created among us animation on home screen

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from '../components/NavBar';
+import AnimatedBg from '../components/AnimatedBg';
+
+function Home(args) {
+  return (
+    <div>
+      <NavBar />
+      <AnimatedBg />
+    </div>
+  );
+}
+
+export default Home;
