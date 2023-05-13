ad68952187f62d089b0d49275ad0f737a5219790
Jaluxe <jcornel00@gmail.com>
2023-05-12 22:06:18 -0700

    NavBar

---
+++ src/NavBar.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function NavBar(args) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar className='navbar navbar-expand-sm'>
+        <NavbarBrand href='/'>reactstrap</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className='me-auto' navbar>
+            <NavItem>
+              <NavLink href='/components/'>Home</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                AP Practice
+              </DropdownToggle>
+              <DropdownMenu right>
+                <NavbarText>Multiple Choice</NavbarText>
+                <DropdownItem>AP Computer Science Principles</DropdownItem>
+                <DropdownItem>AP Computer Science A</DropdownItem>
+                <DropdownItem divider />
+                <NavbarText>Free Response Questions</NavbarText>
+                <DropdownItem>AP Computer Science A</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <NavItem>
+              <NavLink href='https://github.com/reactstrap/reactstrap'>
+                GitHub
+              </NavLink>
+            </NavItem>
+          </Nav>
+
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default NavBar;
