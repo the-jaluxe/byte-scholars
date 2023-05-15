a95ec730b6d62a3e9653f71321dbe9b1dadbd291
Jaluxe <jcornel00@gmail.com>
2023-05-15 03:32:11 -0700

    Finished self-study page

--- src/pages/SelfStudy.js
+++ src/pages/SelfStudy.js
@@ -1,7 +1,94 @@
 import React from 'react';
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+  Button,
+} from 'reactstrap';
+import NavBar from '../components/NavBar';
+import Reviews from '../components/Reviews';
+import TutorTimeline from '../components/TutorTimeline';
+import Footer from '../components/Footer';
+import {
+  faFaceSmile,
+  faGraduationCap,
+  faDollarSign,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import JavaCourse from '../assets/java-course.jpg';
+import PythonCourse from '../assets/python-course.jpg';
 
 function SelfStudy(args) {
-  return <div>self-stud</div>;
+  return (
+    <div class='tutoring'>
+      <NavBar />
+      <Container className='self-container'>
+        <Row>
+          <Col>
+            <h1 class='tutoring-title'>Self-Study Courses</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h4 class='tutoring-subtitle'>
+              Want a self-guided course on coding? Choose from our options
+              below:
+            </h4>
+          </Col>
+        </Row>
+      </Container>
+      <Container className='d-flex course-container'>
+        <Row className='m-auto align-self-center'>
+          <Card>
+            <a href='https://www.udemy.com/course/ap-computer-science-a-java-concepts-and-fundamentals/?referralCode=729147BB9513889EB831'>
+              <img className='img-fluid' alt='Java Course' src={JavaCourse} />
+            </a>
+            <CardBody>
+              <CardTitle tag='h5'>
+                AP Computer Science A - Java Concepts and Fundamentals
+              </CardTitle>
+              <CardSubtitle className='mb-2 text-muted' tag='h6'>
+                Attain a high-level understanding of Java concepts and
+                fundamentals with a focus on succeeding on the AP Exam.
+              </CardSubtitle>
+              <Button href='https://www.udemy.com/course/ap-computer-science-a-java-concepts-and-fundamentals/?referralCode=729147BB9513889EB831'>
+                Visit
+              </Button>
+            </CardBody>
+          </Card>
+        </Row>
+      </Container>
+      <Container className='d-flex course-container'>
+        <Row className='m-auto align-self-center'>
+          <Card>
+            <img
+              className='img-fluid coming-soon'
+              alt='Python Course'
+              src={PythonCourse}
+              width='750'
+              height='422'
+            />
+            <CardBody>
+              <CardTitle tag='h5'>
+                Complete Python Crash Course For Beginners
+              </CardTitle>
+              <CardSubtitle className='mb-2 text-muted' tag='h6'>
+                Learn Python with an easy to follow course featuring hands-on
+                coding exercises and quick videos in under 4 hours.
+              </CardSubtitle>
+              <Button href>Visit</Button>
+            </CardBody>
+          </Card>
+        </Row>
+      </Container>
+      <Footer />
+    </div>
+  );
 }
 
 export default SelfStudy;
