62edcc3ea42c355c92aec73c6bedf229b1085f13
Jaluxe <jcornel00@gmail.com>
2023-05-16 17:05:40 -0700

    Changed App.js to class instead of function

--- src/App.js
+++ src/App.js
@@ -4,21 +4,38 @@ import Home from './pages/Home';
 import Tutoring from './pages/Tutoring';
 import SelfStudy from './pages/SelfStudy';
 import MultipleChoice from './pages/MultipleChoice';
+import { Component } from 'react';
 
-// Add path * for 404 page
-function App() {
-  return (
-    <div className='App'>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/tutoring' element={<Tutoring />} />
-          <Route path='/self-study' element={<SelfStudy />} />
-          <Route path='/multiple-choice' element={<MultipleChoice />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+      questionId: 1,
+      question: '',
+      answerOptions: [],
+      answer: '',
+      answersCount: {},
+      result: '',
+    };
+  }
+
+  // Add path * for 404 page
+  render() {
+    return (
+      <div className='App'>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/tutoring' element={<Tutoring />} />
+            <Route path='/self-study' element={<SelfStudy />} />
+            <Route path='/multiple-choice' element={<MultipleChoice />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
